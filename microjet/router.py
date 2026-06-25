241186d2145e288deb548c5c02e05ff5c198e4d4
CODE GEAR <DuttaArin11@gmail.com>
2026-06-25 14:05:41 +0530

    Delete microjet/router.py

--- microjet/router.py
+++
@@ -1,9 +0,0 @@
-class Router:
-    def __init__(self):
-        self.routes = {}
-
-    def add_route(self, path, component):
-        self.routes[path] = component
-
-    def get(self, path):
-        return self.routes.get(path)
