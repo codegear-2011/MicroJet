0400992250e883765206089c1b6d5896eb30e67f
CODE GEAR <DuttaArin11@gmail.com>
2026-06-25 14:05:16 +0530

    Delete microjet/cli.py

--- microjet/cli.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-def main():
-    parser = argparse.ArgumentParser(prog="atomy")
-    parser.add_argument("command")
-    args = parser.parse_args()
-
-    if args.command == "run":
-        print("Run your app entry point here")
